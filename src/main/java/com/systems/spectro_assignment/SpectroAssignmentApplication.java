825afa0aa45c7873785ff36670a8cb54be75d759
Ahmed Ramadan <ahmednasser1902@gmail.com>
2025-08-26 14:24:07 +0300

    project init

---
+++ src/main/java/com/systems/spectro_assignment/SpectroAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.systems.spectro_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpectroAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpectroAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/systems/spectro_assignment/SpectroAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systems.spectro_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpectroAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
